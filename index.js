e7537573488a217332b6ae83ad2772e23389477c
john joshua <jo214841@gmail.com>
2024-09-08 09:25:14 +0530

    refresh error

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname,'dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'))
-})
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 
 
